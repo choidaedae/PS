f364745aa7c4fe0c31eb2a78d16a293463b4e739
choidaedae <daehyeonchoi@postech.ac.kr>
2024-08-17 00:43:54 +0900

    20240816: solved 2 problems

--- 1149.py
+++ 1149.py
@@ -1,13 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+DP = [[0] * 1001 for _ in range(3)]
+costs = [[0] * 1001 for _ in range(3)]
 
-for i in range(N):
-    DP = [0] * 1001
-    costs = list(map(int, input().strip()))
-    for color, cost in enumerate(costs):
-        DP[color] = cost
-        for i in range(2, 1001):
-            DP[i] = max(DP[i-1])
-    
+N = int(input())
+for i in range(1, N+1):
+    inputs = list(map(int, input().rstrip().split()))
+    for color, cost in enumerate(inputs):
+        costs[color][i] = cost
+for i in range(1, N+1):
+    DP[0][i] = costs[0][i] + min(DP[1][i-1], DP[2][i-1])
+    DP[1][i] = costs[1][i] + min(DP[0][i-1], DP[2][i-1])
+    DP[2][i] = costs[2][i] + min(DP[0][i-1], DP[1][i-1])
+print(min([DP[0][N], DP[1][N], DP[2][N]]))
+       
